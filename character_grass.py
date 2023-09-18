166a7e248e067e3a023c07139620fcdf3fee11a9
BATMINJI <105769332+batminji@users.noreply.github.com>
2023-09-18 12:14:55 +0900

    Drill 02 제출 2022180014 박민지

--- character_grass.py
+++ character_grass.py
@@ -1,19 +1,62 @@
 from pico2d import *
+import math
 
 open_canvas()
 
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-grass.draw_now(400,30)
-character.draw_now(400,90)
+cnt = 0
 
-x = 0
-while(x<800):
-    clear_canvas_now()
-    grass.draw_now(400,30)
-    character.draw_now(x,90)
-    x = x + 2
-    delay(0.01)
+x = 400
+y = 60
+pi = 0
 
-close_canvas()
+while(True):
+    if (cnt == 0):
+        x = 400
+        y = 60
+        while(x < 790 and y == 60):
+            clear_canvas_now()
+            grass.draw_now(400,30)
+            character.draw_now(x,y)
+            x = x + 2
+            delay(0.01)
+        while(x == 790 and y < 540 ):
+            clear_canvas_now()
+            grass.draw_now(400,30)
+            character.draw_now(x,y)
+            y = y + 2
+            delay(0.01)
+        while(x > 10 and y == 540):
+            clear_canvas_now()
+            grass.draw_now(400,30)
+            character.draw_now(x,y)
+            x = x - 2
+            delay(0.01)
+        while (x == 10 and y > 60):
+            clear_canvas_now()
+            grass.draw_now(400,30)
+            character.draw_now(x,y)
+            y = y - 2
+            delay(0.01)
+        while (x < 400 and y == 60):
+            clear_canvas_now()
+            grass.draw_now(400,30)
+            character.draw_now(x,y)
+            x = x + 2
+            delay(0.01)
+        cnt = 1
+
+    if (cnt == 1):
+        pi = 0
+        while(pi<360):
+            x = 400 + 200 * math.cos(math.radians(pi))
+            y = 300 + 200 * math.sin(math.radians(pi))
+            clear_canvas_now()
+            grass.draw_now(400,30)
+            character.draw_now(x,y)
+            pi = pi + 2
+            delay(0.01)
+        cnt = 0
+
